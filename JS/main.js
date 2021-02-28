55e14f8bed537b7c7fae3fc7d7a6e77511f7d272
Areave <just4-spam@mail.ru>
2021-02-28 19:27:40 +0300

    after #7

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,25 @@
         }
     }
 }())
+
+
+(function() {
+    const burger = document.querySelector('.burger');
+    const close = document.querySelector('.header_nav_close');
+    
+    burger.addEventListener('click', () => {
+        console.log(1);
+        const nav = document.querySelector('.header_nav');
+        nav.classList.add('header_nav_active');
+    });
+
+    close.addEventListener('click', () => {        
+        const nav = document.querySelector('.header_nav');
+        nav.classList.remove('header_nav_active');
+        console.log(2);
+    });
+
+}());
+
+
+
